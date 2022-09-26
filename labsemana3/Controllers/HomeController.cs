364bbd5fab407daac6fe70e066806b01b9e13735
MKGKarol <95936570+MKGKarol@users.noreply.github.com>
2022-09-26 15:03:32 -0300

    Projeto MVC

---
+++ labsemana3/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using labsemana3.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace labsemana3.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            var text = new HelloModel();
+            text.Texto = " Hello world MVC";
+            return View(text);
+        }
+    }
+}
---
+++ labsemana3/Models/HelloModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace labsemana3.Models
+{
+    public class HelloModel
+    {
+        public string Texto { get; set; }
+    }
+}
